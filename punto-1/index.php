b9bd28a707b8e1433325483c3511771c61e4fba5
mbarbosa76 <mbarbosa_c_@hotmail.com>
2023-11-21 00:47:06 -0500

    Subir ejercicio 1

---
+++ punto-1/index.php
@@ -0,0 +1,15 @@
+<?php
+  // DEFINICIÓN DE DATOS DE ENTRADA
+  $name = "Miguel Ángel";
+  const HI = "¡Hola!... Bienvenido, ";
+
+  // PROCESO 1
+  echo HI . "";
+  echo $name . "\n";
+
+  $name = "Lenny Alexandra";
+  const HI2 = "¡Hola!... Bienvenida, ";
+    // PROCESO 2
+  echo HI2 . "";
+  echo $name;
+?>
